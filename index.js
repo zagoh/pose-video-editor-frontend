a3b414e98187e0ad0057cb3a4810c256809299c8
zagoh <zhean_93@hotmail.com>
2023-05-24 16:56:01 +0800

    completed frontend design, work on formData next

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
+let selectedFile;
 
-function allowDrop(ev) {
+function dropHandler(ev) {
+    // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
-}
-  
-function drag(ev) {
-    ev.preventDefault();
-    console.log('running drag');
-}
-  
-function drop(ev) {
-    ev.preventDefault();
-    console.log('running drop');
-}
-
-function handleFormSubmit(event) {
-    event.preventDefault();
-    if (selectedFile) {
-        const formData = new FormData();
-        formData.append('video', selectedFile);
-        // You can now send the form data to the server using an AJAX request,
-        // or submit the form to the server using a regular form submit.
-        // Here's an example of how to send the data using an AJAX request:
-        fetch(`${process.env.REACT_APP_API}/upload`, {
-        method: 'POST',
-        body: formData,
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Handle the response from the server
-            const filePath = data.path;
-            console.log(filePath);
-            onReceiveVideo(filePath);
-            // Use the file path as needed in your frontend code
-        })
-        .catch(error => {
-            console.error('Error uploading video:', error);
-        });
-
+    if (ev.dataTransfer.files) {
+        if(ev.dataTransfer.files[0].type == 'video/mp4'){
+            selectedFile = ev.dataTransfer.files[0];
+            document.getElementById('content-exist-info').innerHTML = selectedFile.name + ' has been added.';
+        }
     }
 }
 
-console.log(`${process.env.REACT_APP_API}`);
+function dragOverHandler(ev) {
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+}
+
+
+function handleFormSubmit(ev) {
+    ev.preventDefault();
+    if (selectedFile) {
+        // const formData = new FormData();
+        // formData.append('video', selectedFile);
+        // fetch(`${process.env.REACT_APP_API}/upload`, {
+        // method: 'POST',
+        // body: formData,
+        // })
+        // .then(response => response.json())
+        // .then(data => {
+        //     // Handle the response from the server
+        //     const filePath = data.path;
+        //     console.log(filePath);
+        //     onReceiveVideo(filePath);
+        //     // Use the file path as needed in your frontend code
+        // })
+        // .catch(error => {
+        //     console.error('Error uploading video:', error);
+        // });
+
+        document.getElementById('main-content').classList.add('hidden');
+        document.getElementById('video-content').classList.remove('hidden');
+
+    }
+}
